c747cf312ef2982451b5dcd6189dc7c2542a71be
Masynchin <masynchin@gmail.com>
2021-09-19 23:21:22 +0300

    fix main.py

--- schedule_bot/main.py
+++ schedule_bot/main.py
@@ -1,10 +1,12 @@
 import logging
 import os
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import Message
 from aiogram.dispatcher.filters import Text as TextFilter
 
 from file_service import get_schedule
+import keyboard as kb
 
 
 logging.basicConfig(level=logging.INFO)
@@ -21,8 +23,9 @@ async def send_welcome(message: Message):
     )
 
 
-@dp.message_handler(message)
-async def send_schedule():
+# TODO: add handler filter
+@dp.message_handler()
+async def send_schedule(message: Message):
 	await message.answer(
 		get_schedule(message.text),
 		reply_markup=kb.markup,
