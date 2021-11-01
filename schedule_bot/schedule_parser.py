e29d2cd633f9104c9b56fa8ce2beef2f08376ca2
BobaUbisoft17 <aksud2316@gmail.com>
2021-11-01 20:13:33 +0300

    Added a check for the relevance of the schedule

--- schedule_bot/schedule_parser.py
+++ schedule_bot/schedule_parser.py
@@ -3,13 +3,28 @@ import requests
 from requests import Response
 from bs4 import BeautifulSoup
 from schedule_bot import convert
+from schedule_SQL import init_db
+import glob
+import os
 
 URL = 'https://s11018.edu35.ru/obuchayushchimsya/raspisanie-urokov'
 HEADERS = {
 	'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
 	'accept' : '*/*',
 }
-PATH = 'csv_schedule/'
+
+
+def check_for_innovation(filename: str):
+	csv_files = glob.glob("*.pdf")
+	if csv_files == []:
+		return True
+	else:
+		for file in csv_files:
+			if file != filename:
+				os.remove(file)
+				return True
+			else:
+				return False
 
 
 def get_html(url: str, params: Optional[dict] = None) -> Response:
@@ -26,15 +41,19 @@ def get_schedule_link(html: str) -> Tuple[str, str]:
 
 def get_file(path_and_link: Tuple[str, str]):
 	path, link = (i for i in path_and_link)
-	response = requests.get(link)
-	with open(PATH + path, "wb") as schedule:
-		schedule.write(response.content)
-	convert(path)
+	if check_for_innovation(path):
+		response = requests.get(link)
+		with open(path, "wb") as schedule:
+			schedule.write(response.content)
+		convert(path)
+		return True
 
 
 def parse():
 	html = get_html(URL)
 	if html.status_code == 200:
-		get_file(get_schedule_link(html.text))
+		if get_file(get_schedule_link(html.text)):
+			return True
 	else:
 		print('Error')
+		
