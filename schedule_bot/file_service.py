34330c9a3f6a3bb950617d5f5a519a0bf3ba7c5b
BobaUbisoft17 <aksud2316@gmail.com>
2021-11-03 20:05:13 +0300

    Fix some problems

--- schedule_bot/file_service.py
+++ schedule_bot/file_service.py
@@ -6,7 +6,7 @@ def get_schedule(clas: str) -> str:
     cursor = db.cursor()
     schedules = []
     if len(list(cursor.execute(f"SELECT * FROM schedule WHERE class = '{clas}'"))) == 1:
-        return '\n'.join(list(cursor.execute(f"SELECT * FROM schedule WHERE class = '{clas}'"))[0])
+        return list(cursor.execute(f"SELECT * FROM schedule WHERE class = '{clas}'"))[0][1]
     elif len(list(cursor.execute(f"SELECT * FROM schedule WHERE class = '{clas}'"))) > 1:
         for schedule in cursor.execute(f"SELECT * FROM schedule WHERE class = '{clas}'"):
             schedules.append('\n'.join(schedule))
