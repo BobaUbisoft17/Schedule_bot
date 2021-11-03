2c57d736e6c9c90a900e4b8ac1a48c64ad38f9d5
BobaUbisoft17 <aksud2316@gmail.com>
2021-11-03 20:11:44 +0300

    Change names of functions and write docstrings

--- schedule_bot/schedule_parser.py
+++ schedule_bot/schedule_parser.py
@@ -2,7 +2,7 @@ from typing import Optional, Tuple
 import requests
 from requests import Response
 from bs4 import BeautifulSoup
-from schedule_bot import convert
+from convert_pdf_to_csv import convert
 from schedule_SQL import init_db
 import glob
 import os
@@ -12,6 +12,7 @@ HEADERS = {
 	'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
 	'accept' : '*/*',
 }
+PATH = "schedule_bot/"
 
 
 def check_for_innovation(filename: str):
@@ -28,32 +29,36 @@ def check_for_innovation(filename: str):
 
 
 def get_html(url: str, params: Optional[dict] = None) -> Response:
+	"""Получение кода страницы."""
 	r = requests.get(url, headers=HEADERS, params=params)
 	return r
 
 
-def get_schedule_link(html: str) -> Tuple[str, str]:
-	soup = BeautifulSoup(html, 'lxml')
-	schedules_and_link = {}
+def get_link_and_filename(html_code: str) -> Tuple[str, str]:
+	"""Получение ссылки на файл и названия файла из HTML-кода страницы."""
+	soup = BeautifulSoup(html_code, 'lxml')
 	schedules = soup.find_all('a', class_='at_url')[-1]
 	return schedules.get('href').split("/")[-1], schedules.get('href')
 
 
-def get_file(path_and_link: Tuple[str, str]):
-	path, link = (i for i in path_and_link)
-	if check_for_innovation(path):
+def get_file(filename_and_link: Tuple[str, str]):
+	"""Сохранение файла с расписанием."""
+	filename, link = (i for i in filename_and_link)
+	if check_for_innovation(filename):
 		response = requests.get(link)
-		with open(path, "wb") as schedule:
+		with open(PATH + filename, "wb") as schedule:
 			schedule.write(response.content)
-		convert(path)
+		convert(filename)
 		return True
 
 
 def parse():
+	"""Проверка ответа сервера и запись данных в бд."""
 	html = get_html(URL)
 	if html.status_code == 200:
-		if get_file(get_schedule_link(html.text)):
-			return True
+		if get_file(get_link_and_filename(html.text)):
+			init_db(True)
+			print("Расписание успешно обновлено!")
 	else:
 		print('Error')
 		
