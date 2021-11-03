7fc12f42be618635189423334373d368fe9bcbb0
BobaUbisoft17 <aksud2316@gmail.com>
2021-11-03 20:04:28 +0300

    Added path for file checkout and save

--- schedule_bot/convert_pdf_to_csv.py
+++ schedule_bot/convert_pdf_to_csv.py
@@ -3,6 +3,6 @@ import convertapi
 def convert(filename: str):
     convertapi.api_secret = 'CL1X36TPQhqkEVKa'
     convertapi.convert('csv', {
-        'File': filename,
+        'File': "schedule_bot/" + filename,
         'Delimiter': 'semicolon',
-    }, from_format = 'pdf').save_files()
+    }, from_format = 'pdf').save_files("schedule_bot")
