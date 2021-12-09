807b78a398efac95c5085ff7b69a2379d0e6deeb
BobaUbisoft17 <aksud2316@gmail.com>
2021-12-09 19:54:27 +0300

    Fixed the function to get the next date, fixed the error with saving 2 schedules for one class

--- schedule_bot/convert_text_to_image.py
+++ schedule_bot/convert_text_to_image.py
@@ -17,15 +17,17 @@ async def make_image(date):
     """Фунция для создания изображения с расписанием."""
     await del_img()
     list_10_11 = []
-    date = await get_date(date)
+    date_first_number = await get_date(date)
+    next_date = await get_next_date(date)
     for class_ in await get_classes_schedules():
         class_name = class_.class_name
         list_schedule = class_.schedule
         bells = class_.bells
 
         if class_name in list_10_11:
-            date = await get_next_date(date)
+            date = next_date
         else:
+            date = date_first_number
             list_10_11.append(class_name)
         # create an image
         out = Image.new("RGB", (1442, 1600), "white")
@@ -74,9 +76,10 @@ async def make_image(date):
 
 
 
-        filename = f"{PATH}{class_name}.jpg"
-        if filename in glob.glob(PATH):
-            out.save(PATH + class_name + "_st.jpg")
+        filename = f"{class_name}.jpg"
+        schedules = [path.split("\\")[-1] for path in glob.glob(PATH + "*.jpg")]
+        if filename in schedules:
+            out.save(PATH + class_name + "2.jpg")
         else:
             out.save(PATH + class_name + ".jpg")
 
@@ -85,44 +88,49 @@ async def get_date(date):
     """Функция для получения даты расписания."""
     if len(date[0].split(".")) == 2:
         day, month = date[0].split(".")
-        year = ""
+        year = "       "
+        return f"{day} {await get_month(int(month))} {year}"
     else:
         day, month, year = date[0].split(".")
-    return f"{day} {await get_month(int(month))} 20{year}"
+        return f"{day} {await get_month(int(month))} 20{year}"
 
 
 async def get_next_date(date):
     """Функция для получения даты расписания + 1 день."""
 
     """В основном используется для получения даты расписания на субботу."""
+    month_31 = [1, 3, 5, 7, 8, 10, 12]
     if len(date[0].split(".")) == 2:
         day, month = date[0].split(".")
-        year = ""
+        year = "       "
     else:
         day, month, year = date[0].split(".")
-    month_31 = [1, 3, 5, 7, 8, 10, 12]
     if month in month_31:
-        if 1 <= int(day) + 1 < 31:
-            return int(day) + 1, await get_month(int(month)), year
+        if 1 <= int(day) + 1 <= 31:
+            if year.replace(" ", "") != "":
+                return f"{int(day) + 1} {await get_month(int(month))} 20{year}"
+            else:
+                return f"{int(day) + 1} {await get_month(int(month))} {year}"
         else:
-            if int(month) == 12:
-                return 1, await get_month(1), int(year) + 1
-            else:
-                return 1, await get_month(int(month) + 1), year
+            return f"1 {await get_month(int(month) + 1)} {year}"
     else:
-        if 0 <= int(day) + 1 <= 30 and int(month) != 2:
-            return int(day) + 1, await get_month(int(month)), year
-        elif int(month) == 2:
-            if (int("20" + year) % 4 == 0) or (int("20" + year) % 400 == 0 and int("20" + year) % 100 == 0):
-                if int(day) < 29:
-                    return int(day) + 1, await get_month(int(month)), year
+        if year.replace(" ", "") != "": 
+            if 0 <= int(day) + 1 <= 30 and int(month) != 2:
+                return f"{int(day) + 1} {await get_month(int(month))} 20{year}"
+            elif int(month) == 2:
+                if (int("20" + year) % 4 == 0) or (int("20" + year) % 400 == 0 and int("20" + year) % 100 == 0):
+                    if int(day) < 29:
+                        return f"{int(day) + 1} {await get_month(int(month))} 20{year}"
+                    else:
+                        return f"1 {await get_month(int(month) + 1)} 20{year}"
                 else:
-                    return 1, await get_month(int(month) + 1), year
-            else:
-                if int(day) < 28:
-                    return int(day) + 1, await get_month(int(month)), year
-                else:
-                    return 1, await get_month(int(month) + 1), year
+                    if int(day) < 28:
+                        return f"{int(day) + 1} {await get_month(int(month))} 20{year}"
+                    else:
+                        return f"1 {await get_month(int(month) + 1)} 20{year}"
+        else:
+            if 0 <= int(day) + 1 <= 30 and int(month) != 2:
+                return f"{int(day) + 1} {await get_month(int(month))} {year}"
 
 
 async def get_month(number_of_month):
