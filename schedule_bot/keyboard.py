1fd5cfce5b36fc785e54eeece0af1e1a5edd6a3a
BobaUbisoft17 <aksud2316@gmail.com>
2022-01-08 22:43:13 +0300

    Add new button and keyboards

--- schedule_bot/keyboard.py
+++ schedule_bot/keyboard.py
@@ -1,4 +1,5 @@
-from vkwave.bots.utils.keyboards.keyboard import ButtonColor, Keyboard
+from types import TracebackType
+from vkbottle import Keyboard, KeyboardButtonColor, Text
 
 
 parallel = [
@@ -31,37 +32,46 @@ kb_choice_parallel = Keyboard(one_time=True)
 kb_choice_class = Keyboard(one_time=True)
 kb_subscribe_to_newsletter = Keyboard(one_time=True)
 kb_unsubscribe_from_mailing_list = Keyboard(one_time=True)
+kb_memory_class = Keyboard(one_time=True)
+kb_change_class = Keyboard(one_time=True)
 
 
-kb_get_schedule.add_text_button("Узнать расписание", color=ButtonColor.POSITIVE)
-kb_get_schedule.add_row()
-kb_get_schedule.add_text_button("Настроить уведомления", color=ButtonColor.SECONDARY)
-kb_subscribe_to_newsletter.add_text_button(
-    "Подписаться на рассылку", color=ButtonColor.PRIMARY
+kb_get_schedule.add(Text("Узнать расписание"), color=KeyboardButtonColor.POSITIVE)
+kb_get_schedule.row()
+kb_get_schedule.add(Text("Настроить уведомления"), color=KeyboardButtonColor.SECONDARY)
+kb_get_schedule.row()
+kb_get_schedule.add(Text("Настроить запоминание класса"), color=KeyboardButtonColor.SECONDARY)
+kb_subscribe_to_newsletter.add(Text(
+    "Подписаться на рассылку"), color=KeyboardButtonColor.PRIMARY
 )
-kb_subscribe_to_newsletter.add_row()
-kb_subscribe_to_newsletter.add_text_button("назад", color=ButtonColor.NEGATIVE)
-kb_unsubscribe_from_mailing_list.add_text_button(
-    "Отписаться от рассылки", color=ButtonColor.PRIMARY
+kb_subscribe_to_newsletter.row()
+kb_subscribe_to_newsletter.add(Text("Назад"), color=KeyboardButtonColor.NEGATIVE)
+kb_unsubscribe_from_mailing_list.add(Text(
+    "Отписаться от рассылки"), color=KeyboardButtonColor.PRIMARY
 )
-kb_unsubscribe_from_mailing_list.add_row()
-kb_unsubscribe_from_mailing_list.add_text_button("назад", color=ButtonColor.NEGATIVE)
+kb_unsubscribe_from_mailing_list.row()
+kb_unsubscribe_from_mailing_list.add(Text("Назад"), color=KeyboardButtonColor.NEGATIVE)
 
 
 for i in range(len(parallel)):
     if (i + 1) % 3 == 0:
-        kb_choice_parallel.add_row()
-    kb_choice_parallel.add_text_button(parallel[i], color=ButtonColor.POSITIVE)
+        kb_choice_parallel.row()
+    kb_choice_parallel.add(Text(parallel[i]), color=KeyboardButtonColor.POSITIVE)
 
 
-def give_parallel(parallel):
+async def give_parallel(parallel):
     """Функция для генерации клавиатуры."""
     kb_choice_class = Keyboard(one_time=True)
     count = 0
     for class_ in CLASSES_NAMES:
         if parallel in class_:
             if count % 4 == 0 and count != 0:
-                kb_choice_class.add_row()
+                kb_choice_class.row()
             count += 1
-            kb_choice_class.add_text_button(class_, color=ButtonColor.POSITIVE)
+            kb_choice_class.add(Text(class_), color=KeyboardButtonColor.POSITIVE)
     return kb_choice_class
+
+kb_memory_class.add(Text("Запомнить мой класс"), color=KeyboardButtonColor.POSITIVE)
+kb_change_class.add(Text("Изменить класс"), color=KeyboardButtonColor.POSITIVE)
+kb_change_class.row()
+kb_change_class.add(Text("Удалить данные о моём классе"), color=KeyboardButtonColor.NEGATIVE)
