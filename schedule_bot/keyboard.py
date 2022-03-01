b5fbd8bba5a305c5bdc94989145e3fd49563c725
BobaUbisoft17 <aksud2316@gmail.com>
2022-03-01 21:22:09 +0300

    Code refactoring

--- schedule_bot/keyboard.py
+++ schedule_bot/keyboard.py
@@ -27,67 +27,101 @@ CLASSES_NAMES = [
 ]
 
 
-kb_get_schedule = Keyboard(one_time=True)
+kb_get_schedule = (
+    Keyboard(one_time=True)
+    .add(
+        Text("Узнать расписание", {"cmd": "get_schedule"}),
+        color=KeyboardButtonColor.POSITIVE,
+    )
+    .row()
+    .add(Text("Настройки", {"cmd": "settings"}), color=KeyboardButtonColor.SECONDARY)
+    .row()
+    .add(
+        OpenLink(
+            "https://vk.com/@schedulebot14-pomosch-v-poluchenii-raspisaniya", "Помощь"
+        ),
+        color=KeyboardButtonColor.SECONDARY,
+    )
+)
+
+kb_subscribe_to_newsletter = (
+    Keyboard()
+    .add(
+        Text("Подписаться на рассылку", {"cmd": "subscribe_on_newsletter"}),
+        color=KeyboardButtonColor.POSITIVE,
+    )
+    .row()
+    .add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
+)
+
+
+kb_unsubscribe_from_mailing_list = (
+    Keyboard()
+    .add(
+        Text("Отписаться от рассылки", {"cmd": "unsubscribe_on_newsletter"}),
+        color=KeyboardButtonColor.NEGATIVE,
+    )
+    .row()
+    .add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
+)
+
+
+kb_memory_class = (
+    Keyboard(one_time=True)
+    .add(
+        Text("Запомнить мой класс", {"cmd": "memory_my_class"}),
+        color=KeyboardButtonColor.POSITIVE,
+    )
+    .row()
+    .add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
+)
+
+
+kb_change_class = (
+    Keyboard(one_time=True)
+    .add(
+        Text("Изменить класс", {"cmd": "change_my_class"}),
+        color=KeyboardButtonColor.POSITIVE,
+    )
+    .row()
+    .add(
+        Text("Удалить данные о моём классе", {"cmd": "del_my_class"}),
+        color=KeyboardButtonColor.NEGATIVE,
+    )
+    .row().add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
+)
+
+
+kb_settings = (
+    Keyboard()
+    .add(
+        Text("Настроить уведомления", {"cmd": "set_notifications"}),
+        color=KeyboardButtonColor.SECONDARY,
+    )
+    .row()
+    .add(
+        Text("Запоминание класса", {"cmd": "set_memory_class"}),
+        color=KeyboardButtonColor.SECONDARY,
+    )
+    .row()
+    .add(Text("Назад", {"cmd": "back2"}), color=KeyboardButtonColor.PRIMARY)
+)
+
+
 kb_choice_parallel = Keyboard()
-kb_choice_class = Keyboard()
-kb_subscribe_to_newsletter = Keyboard()
-kb_unsubscribe_from_mailing_list = Keyboard()
-kb_memory_class = Keyboard(one_time=True)
-kb_change_class = Keyboard(one_time=True)
-kb_settings = Keyboard()
-
-
-kb_get_schedule.add(
-    Text("Узнать расписание", {"cmd": "get_schedule"}),
-    color=KeyboardButtonColor.POSITIVE,
-)
-kb_get_schedule.row()
-kb_get_schedule.add(
-    Text("Настройки", {"cmd": "settings"}), color=KeyboardButtonColor.SECONDARY
-)
-kb_get_schedule.row()
-kb_get_schedule.add(
-    OpenLink(
-        "https://vk.com/@schedulebot14-pomosch-v-poluchenii-raspisaniya", "Помощь"
-    ),
-    color=KeyboardButtonColor.SECONDARY,
-)
-kb_settings.add(
-    Text("Настроить уведомления", {"cmd": "set_notifications"}),
-    color=KeyboardButtonColor.SECONDARY,
-)
-kb_settings.row()
-kb_settings.add(
-    Text("Запоминание класса", {"cmd": "set_memory_class"}),
-    color=KeyboardButtonColor.SECONDARY,
-)
-kb_settings.row()
-kb_settings.add(Text("Назад", {"cmd": "back2"}), color=KeyboardButtonColor.PRIMARY)
-kb_subscribe_to_newsletter.add(
-    Text("Подписаться на рассылку", {"cmd": "subscribe_on_newsletter"}),
-    color=KeyboardButtonColor.POSITIVE,
-)
-kb_subscribe_to_newsletter.row()
-kb_subscribe_to_newsletter.add(
-    Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY
-)
-kb_unsubscribe_from_mailing_list.add(
-    Text("Отписаться от рассылки", {"cmd": "unsubscribe_on_newsletter"}),
-    color=KeyboardButtonColor.NEGATIVE,
-)
-kb_unsubscribe_from_mailing_list.row()
-kb_unsubscribe_from_mailing_list.add(
-    Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY
-)
-
-
 for i in range(len(parallel)):
     if (i + 1) % 3 == 0:
         kb_choice_parallel.row()
     kb_choice_parallel.add(
         Text(parallel[i], {"cmd": "parallel"}), color=KeyboardButtonColor.POSITIVE
     )
-kb_choice_parallel.row().add(Text("Назад", {"cmd": "back3"}), color=KeyboardButtonColor.PRIMARY)
+kb_choice_parallel.row().add(
+    Text("Назад", {"cmd": "back3"}), color=KeyboardButtonColor.PRIMARY
+)
+
+
+kb_choice_class = Keyboard()
+
 
 async def give_parallel(parallel):
     """Функция для генерации клавиатуры."""
@@ -101,24 +135,7 @@ async def give_parallel(parallel):
             kb_choice_class.add(
                 Text(class_, {"cmd": "class_"}), color=KeyboardButtonColor.POSITIVE
             )
-    kb_choice_class.row().add(Text("Назад", {"cmd": "back4"}), color=KeyboardButtonColor.PRIMARY)
+    kb_choice_class.row().add(
+        Text("Назад", {"cmd": "back4"}), color=KeyboardButtonColor.PRIMARY
+    )
     return kb_choice_class
-
-
-kb_memory_class.add(
-    Text("Запомнить мой класс", {"cmd": "memory_my_class"}),
-    color=KeyboardButtonColor.POSITIVE,
-)
-kb_memory_class.row()
-kb_memory_class.add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
-kb_change_class.add(
-    Text("Изменить класс", {"cmd": "change_my_class"}),
-    color=KeyboardButtonColor.POSITIVE,
-)
-kb_change_class.row()
-kb_change_class.add(
-    Text("Удалить данные о моём классе", {"cmd": "del_my_class"}),
-    color=KeyboardButtonColor.NEGATIVE,
-)
-kb_change_class.row()
-kb_change_class.add(Text("Назад", {"cmd": "back1"}), color=KeyboardButtonColor.PRIMARY)
