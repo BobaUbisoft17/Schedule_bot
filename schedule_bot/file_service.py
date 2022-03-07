aca58b2b637d06b6afaeee6cb4a28e4ab0cd18c5
BobaUbisoft17 <aksud2316@gmail.com>
2022-03-07 18:23:22 +0300

    Add dict with filepath to several school

--- schedule_bot/file_service.py
+++ schedule_bot/file_service.py
@@ -1,5 +1,11 @@
 import glob
 
 
-async def get_schedule_class(classname):
-    return [file for file in glob.glob("schedule_image/*.jpg") if classname in file]
+dict_of_schoolpath = {
+    "14": "school14",
+    "40": "school40",
+}
+
+
+async def get_schedule_class(school, classname):
+    return [file for file in glob.glob("schedule_image/" + dict_of_schoolpath[school] + "/*.jpg") if classname in file]
