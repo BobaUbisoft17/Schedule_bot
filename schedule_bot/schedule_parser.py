aea4ec04604f326b2d7ac973103c75b13820342f
BobaUbisoft17 <aksud2316@gmail.com>
2022-06-27 14:09:07 +0300

    Deleted extra file

--- schedule_bot/schedule_parser.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from schedule_parser14 import parse
-
-
-parsers = [parse]
-async def schedule_parser(bot):
-    while True:
-        for parser in parsers:
-            await parser(bot)
