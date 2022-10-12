d9aea3c83ee3ce7d1cfcf2c6ffd4163d92638c21
BobaUbisoft17 <aksud2316@gmail.com>
2022-10-12 21:44:27 +0300

    Changed check date valid

--- schedule_bot/utils.py
+++ schedule_bot/utils.py
@@ -1,16 +1,11 @@
 import datetime 
-from typing import List
 
 
 def get_date(string: str) -> str:
+    string = string.replace("_", " ")
     for elem in string.split():
         if is_date(elem):
             return elem
-
-    for elem in string.split("_"):
-        date = elem.split()[0]
-        if is_date(elem.split()[0]):
-            return date
 
 
 def is_date(string: str) -> bool:
