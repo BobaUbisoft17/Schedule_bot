7c24152fb2593aa2d87397bc5ddf83465d4acf04
BobaUbisoft17 <aksud2316@gmail.com>
2022-12-12 19:13:33 +0300

    Changed way to parse date

--- schedule_bot/utils.py
+++ schedule_bot/utils.py
@@ -6,16 +6,28 @@ import datetime
 def get_date(string: str) -> str:
     """Поиск даты в строке."""
     string = string.replace("_", " ")
+    string = string[::-1].replace(".", " ", 1)[::-1]
     for elem in string.split():
-        if is_date(elem):
+        if is_short_date(elem):
             return elem
+        elif is_long_date(elem):
+            return elem[:-4] + elem[-2:]
     return None
 
 
-def is_date(string: str) -> bool:
-    """Проверка на соответствие шаблону."""
+def is_short_date(string: str) -> bool:
+    """Проверка на соответствие короткому шаблону."""
     try:
         datetime.datetime.strptime(string, "%d.%m.%y")
         return True
     except ValueError:
         return False
+
+def is_long_date(string: str) -> bool:
+    """Проверка на соответствие длинному шаблону."""
+    try:
+        datetime.datetime.strptime(string, "%d.%m.%Y")
+        return True
+    except:
+        return False
+
